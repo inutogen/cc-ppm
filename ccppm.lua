6164c35894ce4b36b900d0b1017066b0b097dc67
inutogen <153481480+inutogen@users.noreply.github.com>
2024-02-05 00:16:33 -0600

    Update ccppm.lua

--- ccppm.lua
+++ ccppm.lua
@@ -142,4 +142,88 @@ if args[1] == "from-ppm" then
 		end
 		pic.write("\n")
 	end
+elseif args[1] == "to-ppm" then
+	local nfp = fs.open(args[2], "r")
+	local ppm = fs.open(args[3], "w")
+	ppm.write("P3\n")
+	local data = nfp.readLine()
+	local leng = #data
+	nfp.close()
+	local nfp = fs.open(args[2], "r")
+	local hcount = 0
+	repeat
+		data = nfp.readLine()
+		if data ~= nil then
+			hcount = hcount + 1
+		end
+	until data == nil
+
+	ppm.write(leng.." "..hcount.."\n")
+	ppm.write("255\n")
+	
+	local nfp = fs.open(args[2], "r")
+
+	local done = false
+	repeat 
+		local data = nfp.readLine()
+		if data == nil then
+			done = true
+		else
+			local chars = {}
+    			str:gsub(".", function(c) table.insert(chars, c) end)
+			for i,v in ipairs(chars) do
+				if v == "0" then
+					ppm.write("240 240 240 ")
+				end
+				if v == "0" then
+					ppm.write("242 178 51 ")
+				end
+				if v == "0" then
+					ppm.write("229 127 216 ")
+				end
+				if v == "0" then
+					ppm.write("153 178 242 ")
+				end
+				if v == "0" then
+					ppm.write("222 222 108 ")
+				end
+				if v == "0" then
+					ppm.write("127 204 25 ")
+				end
+				if v == "0" then
+					ppm.write("242 178 204 ")
+				end
+				if v == "0" then
+					ppm.write("76 76 76 ")
+				end
+				if v == "0" then
+					ppm.write("153 153 153 ")
+				end
+				if v == "0" then
+					ppm.write("76 153 178 ")
+				end
+				if v == "0" then
+					ppm.write("178 102 229 ")
+				end
+				if v == "0" then
+					ppm.write("51 102 204 ")
+				end
+				if v == "0" then
+					ppm.write("127 102 76 ")
+				end
+				if v == "0" then
+					ppm.write("87 166 78 ")
+				end
+				if v == "0" then
+					ppm.write("204 76 76 ")
+				end
+				if v == "0" then
+					ppm.write("17 17 17 ")
+				end
+			end
+			ppm.write("\n")
+		end
+	until done == true
+else
+	print("Usuage: \nccppm from-ppm [from .ppm] [to .nfp]\nccppm to-ppm [from .nfp] [to .ppm]")
 end
