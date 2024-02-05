6c44d27cbec4f4af059d22d2f0df5586e9ca0b71
inutogen <153481480+inutogen@users.noreply.github.com>
2024-02-04 23:11:47 -0600

    Update ccppm.lua

--- ccppm.lua
+++ ccppm.lua
@@ -1,5 +1,5 @@
 local args = {...}
-if args[1] == "from-ppm"
+if args[1] == "from-ppm" then
   local ppm = fs.open(args[2], "r")
   for i=1,3 do
     ppm.readLine()
@@ -10,24 +10,27 @@ if args[1] == "from-ppm"
 	repeat
 		maini = maini + 1
 		line = ppm.readLine()
-		if line == "" then
+		if line == nil then
 			done = true
 		else
-			tab = textutils.unserialise(line)
-			newTab = {}
-			icount = 0
+			local tab = {}
+   			for word in line:gmatch("%w+") do table.insert(tab, word) end
+   			print(tab, line)
+			local newTab = {}
+			local icount = 0
 			for i=1,#tab,3 do
 				icount = icount + 1
 				table.insert(newTab, icount, {tab[i], tab[i+1], tab[i+2]})
 			end
 			table.insert(mainTab, maini, newTab)
 		end
-  until done == true
+  	until done == true
 	for i,v in ipairs(mainTab) do
 		for i1,v1 in ipairs(v) do
-			for i2,v2 in ipairs(v1) do
-				print(textutils.serialise(v2))
-			end
+			local prgb = vector.new(v1[1], v1[2], v1[3])
+			local test = vector.new(130, 130, 130)
+			local distance = (prgb-test):length()
+			print(distance)
 		end
 	end
 end
